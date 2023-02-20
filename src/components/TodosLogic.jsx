116832cf30b4d8746811c953953c152bbef6fab0
Adeshina Adenuga <Nuga0909@gmail.com>
2023-02-20 18:09:10 +0100

    Add form functionality

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,32 +1,47 @@
 import InputTodo from "./InputTodo";
 import TodosList from "./TodosList";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const TodosLogic = () => {
   const [todos, setTodos] = useState([
-  {
-    id: 1,
-    title: 'Setup development environment',
-    completed: true,
-  },
-  {
-    id: 2,
-    title: 'Develop website and add content',
-    completed: false,
-  },
-  {
-    id: 3,
-    title: 'Deploy to live server',
-    completed: false,
-  },
-]);
-const delTodo = (id) => {
-  console.log('deleted', id);
-};
+    {
+      id: uuidv4(),
+      title: "Setup development environment",
+      completed: true,
+    },
+    {
+      id: uuidv4(),
+      title: "Develop website and add content",
+      completed: false,
+    },
+    {
+      id: uuidv4(),
+      title: "Deploy to live server",
+      completed: false,
+    },
+  ]);
+  const delTodo = (id) => {
+    setTodos([
+      ...todos.filter((todo) => {
+        return todo.id !== id;
+      }),
+    ]);
+  };
+
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+  
   return (
     <div>
-      <InputTodo />
-      <TodosList todosProps={todos} setTodos={setTodos} delTodo={delTodo}/>
+      <InputTodo addTodoItem={addTodoItem} />
+      <TodosList todosProps={todos} setTodos={setTodos} delTodo={delTodo} />
     </div>
   );
 };
